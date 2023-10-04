553c524dadd34a4a958f332b5912c53cb3979631
csm <csm009177@gamil.com>
2023-10-04 17:49:20 +0900

    식사전 마무리

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 import http.server
 
-httpd = http.server.HTTPServer(('1.1.1.1', 8080), http.server.SimpleHTTPRequestHandler)
+httpd = http.server.HTTPServer(('0.0.0.0', 8080), http.server.SimpleHTTPRequestHandler)
 
 httpd.serve_forever()
