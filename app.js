6a9828ab1edada3221703ba878ab58ca098a450f
csm <csm009177@gmail.com>
2023-11-23 13:41:19 +0900

    test dt13-1

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
-const workVariable = {
-  first : '1',
-  second : '2.5',
+const fs = require('fs');
+const path = require('path');
+
+// 파일이 .txt 파일인지 확인하는 함수
+const isTxtFile = (fileName) => {
+  // 경로의 확장자를 반환한다
+  let result = path.extname(fileName).toLowerCase() === '.txt';
+  return result; // 문자열이다.
 };
 
-function convertToIntegers (obj) {
-  const result = {};
-  
-  for(const key in obj) {
-    try {
-      const valueAsInteger = parseInt(obj[key], 10);
-      if (valueAsInteger.toString() !== obj[key]) {
-        throw new Error(`${obj[key]}은(는) 정수로 정확하게 변환될 수 없습니다`);
-      }
-      result[key] = valueAsInteger;
-    } catch (error) {
-      console.log(`"${key}" 속성 처리 중 오류 발생 : ${error.message}`);
-    }
+// 두 .txt 파일의 내용을 비교하는 함수
+const compareFiles = (operator, operand) => {
+  try {//예외처리
+  // 파일 확장자 검사
+  if(!isTxtFile(operator) || !isTxtFile(operand)) {
+    console.error('둘다 .txt 파일이 아닙니다.');
+    return false;
   }
-  return result;
+  // 파일의 전체 경로를 생성
+  // __dirname은 현재 파일의 위치를 나타낸다
+  const filePathOperator = path.join(__dirname, 'operator.json');
+  const filePathOperand = path.join(__dirname, 'operand.json');
+
+  // 순서대로(동기적으로) 파일을 읽게 접미사인 Sync를 붙였다. (Node.js에서 Sync는 동기적으로 실행한다는 의미이다)
+  const dataOperator = fs.readFileSync(filePathOperator, 'utf8');
+  const dataOperand = fs.readFileSync(filePathOperand, 'utf8');
+
+  let isResult = dataOperator === dataOperand;
+  // 파일의 내용을 비교하여 결과를 반환한다
+  return isResult;
+  }catch (error) {
+    console.error('Error reading files :', error);
+    return false;
+  }
 }
 
-const convertedValues = convertToIntegers(workVariable);
-console.log(convertedValues)
+// 함수 사용 예
+const areFilesEqual = compareFiles('operator.txt', 'operand.txt');
+console.log(areFilesEqual); // 내용이 같다면 true, 다르다면 false를 출력한다
